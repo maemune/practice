b38b4118bf73a62783536a4d8076589199577f28
maemune <115972024+maemune@users.noreply.github.com>
2023-04-20 01:25:15 +0900

    Update setup.sh

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -7,3 +7,5 @@ echo 'ubuntu ALL=NOPASSWD: ALL' | EDITOR='tee -a' visudo
 ufw allow 22
 ufw enable
 chmod 700 ssh_setting.sh && ./ssh_setting.sh
+adduser -q --gecos "" --disabled-login ubuntu
+gpasswd -a ubuntu sudo
