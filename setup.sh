989d3261486cb096437d62c0065d9de3a53d1c5c
maemune <115972024+maemune@users.noreply.github.com>
2023-04-20 01:14:43 +0900

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt-get update
-apt full-upgrade -y
-apt autoremove -y
-timedatectl set-timezone Asia/Tokyo
-echo 'ubuntu ALL=NOPASSWD: ALL' | EDITOR='tee -a' visudo
-ufw allow 22
-ufw enable
-chmod 700 ssh_setting.sh && ./ssh_setting.sh
