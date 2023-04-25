57d0980e4b3df39c126f03723d538af5514f873f
maemune <115972024+maemune@users.noreply.github.com>
2023-04-25 19:15:01 +0900

    Update python_setup.sh

--- ubuntu/python_setup.sh
+++ ubuntu/python_setup.sh
@@ -8,4 +8,5 @@ sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.10 0
 sudo apt-get install -y python3.10-dev python3-pip python3.10-distutils
 curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
 python -m pip install --upgrade pip
-sudo apt install -y mysql-client libmysqlclient-dev libpango-1.0-0 libpangoft2-1.0-0
+sudo apt install -y mysql-server mysql-client libmysqlclient-dev libpango-1.0-0 libpangoft2-1.0-0
+python -m pip install -r requirements.txt
